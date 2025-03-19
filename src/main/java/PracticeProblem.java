110adfd5b9ee25dcc2995c001e26b25b990cfeb3
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-03-19 15:44:35 +0000

    commit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,36 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static boolean validIndex (int[] array, int index){
+		try{
+			int valid = array[index];
+			return true;
+		}catch(ArrayIndexOutOfBoundsException e){
+			return false;
+		}
+		
+	}
+
+	public static int divide (int num, int num2){
+		try{
+			int ans = (num/num2);
+			return ans;
+		}catch(ArithmeticException e){
+			return 0;
+
+		}
+	}
+
+	public static int safeConvertStringtoInt (String word){
+		try{
+			int num = Integer.parseInt(word);
+			return num; 
+
+		}catch (NumberFormatException e){
+			return 0;
+		}
+	}
+
+	// Hi Mr.Kalisz
 
 }
